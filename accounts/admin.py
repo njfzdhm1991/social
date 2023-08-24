02d340fd3f881e5d94d9cbd433df1e78013e8413
m najafzadeh <njfzdhm1991@gmail.com>
2023-08-24 13:35:13 +0330

    account app deployment

---
+++ accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from accounts.models import Profile, Country
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone_number']
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'abbr', 'is_active']
